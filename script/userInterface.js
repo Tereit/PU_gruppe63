d7fe2553c307a6b277f448d889fd451f151eaeda
Martin Halvor Kostveit <martin_kostveit@hotmail.com>
2017-02-13 15:28:27 +0100

    added userInterface, made user

---
+++ script/userInterface.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Martin Kostveit on 13.02.2017.
+ */
+
+function User(name) {
+    this.name=name;
+}
+
+var testUser = new User("martin");
+console.log(testUser);
+console.log(testUser.name);
