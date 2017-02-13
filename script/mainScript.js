1de2f7511bceee889973613737b488114c83edf8
Morten Mulvik <mortenmu@stud.ntnu.no>
2017-02-13 14:17:57 +0100

    Added initFirebase()
    Created a basic Lecturify object and added an initFirebase() function.

--- script/mainScript.js
+++ script/mainScript.js
@@ -1,3 +1,21 @@
 /**
  * Created by Martin Kostveit on 13.02.2017.
  */
+
+'use strict';
+
+function Lecturify() {
+	
+	
+	
+	this.initFirebase();
+}
+
+Lecturify.prototype.initFirebase = function() {
+	// Shortcuts to Firebase SDK features
+	this.auth = firebase.auth();
+	this.database = firebase.database();
+	this.storage = firebase.storage();
+	// Initiates Firebase auth and listen to auth state changes.
+	this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
+};
