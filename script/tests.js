df18805af8895e63d2d43a1452206df5f6fb065c
Morten Mulvik <mortenmu@stud.ntnu.no>
2017-02-15 16:14:14 +0100

    User story 2
    First test for user story 2 kind of works, need to revisit after
    gathering more info about testing of asynchronous data

--- script/tests.js
+++ script/tests.js
@@ -1,5 +1,4 @@
-
-QUnit.test( "User story 1 tests", function( assert ) {
+QUnit.test("User story 1 tests", function(assert) {
 	var testRef = "subjects/algdat/lecture1/pace";
 	function getPace() {
 		var ref = firebase.database().ref(testRef);
@@ -7,18 +6,37 @@ QUnit.test( "User story 1 tests", function( assert ) {
 			return snapshot.val();
 		});
 	}
-	getPace();
 	var originalPace = getPace();
 	var increasedPace = originalPace + 1;
-	lecturify.speedUp("subjects/algdat/lecture1/pace");
+	lecturify.speedUp(testRef);
 	var increasedPace = getPace();
 	assert.equal(increasedPace, increasedPace);
-	lecturify.slowDown("subjects/algdat/lecture1/pace");
+	lecturify.slowDown(testRef);
 	var slowedPace = getPace();
 	assert.equal(slowedPace, originalPace);
 });
 
+QUnit.test("User story 2 tests", function(assert) {
+	//TODO: asynchronous data causing issues for the test
+	var done = assert.async();
+	var testRef = firebase.database().ref();
+	var testUsername = "martin@stud.ntnu.no";
+	function checkUser() {
+		testRef.child("users/professors").once('value').then(function(snapshot) {
+			snapshot.forEach(function(childSnapshot) {
+				if(childSnapshot.val().username === testUsername) {
+					assert.ok(true);
+					done();
+				}
+			});
+		}, function(error) {
+			assert.ok(false);
+		});
+	}
+	checkUser();
+});
+
 window.onload = function() {
-	  window.lecturify = new Lecturify();
-	  //this.firebase = new Firebase("lecturify.firebaseapp.com");
+	window.lecturify = new Lecturify();
+	// this.firebase = new Firebase("lecturify.firebaseapp.com");
 };
