a9fc74fce58cc7a26831c33b56c25b185ba0668e
Mathias <Mathias@10.22.42.48>
2017-02-15 16:01:48 +0100

    Subscriptionlogic trialFunction works for request data, and displaying

--- script/subscriptionLogic.js
+++ script/subscriptionLogic.js
@@ -30,3 +30,26 @@ function fetchLectures(user){
 		});
 	}
 }
+
+function trialFunction(){
+	var dbRef = firebase.database().ref();
+	var arrayOfLectures = [];
+	dbRef.child("users/students/X3SJ2TMw3yMwz04fPoxSSuZCxW13/subscriptions").once("value", function(snapshot){
+		var out = "";
+		console.log("#1");
+		console.log(snapshot.val());
+		snapshot.forEach(function(childSnap){
+			console.log("#2");
+			console.log(childSnap.val());
+			out += childSnap.val().id + "\n";
+			var td = document.createElement("TD");
+			var tr = document.createElement("TR");
+			var t = document.createTextNode(childSnap.val().id);
+			td.appendChild(t);
+			tr.appendChild(td);
+			subjectTableId.appendChild(tr);
+		});
+		
+		document.getElementById("textAreaID").innerHTML = out;
+	});
+}
