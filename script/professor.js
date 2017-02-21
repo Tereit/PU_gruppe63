c1b9d9e8bdc47e69e606e479a6c3a022e5df1910
Martin Halvor Kostveit <martin_kostveit@hotmail.com>
2017-02-21 17:22:06 +0100

    fixed function

--- script/professor.js
+++ script/professor.js
@@ -7,6 +7,11 @@ console.log(type)
 function addSubject() {
     var liElement = document.createElement("li");
     liElement.innerHTML=document.getElementById("newSubjectName").value;
+    liElement.onclick="chooseSubject("+this.innerHTML+")";
     document.getElementById("subjectList").appendChild(liElement);
     /*TODO(make server handle new subject)*/
+}
+
+function chooseSubject(subjectName) {
+    console.log(subjectName);
 }
