4bcc8dd48c033754258af641a1aeccf2b5eb57ab
Martin Halvor Kostveit <martin_kostveit@hotmail.com>
2017-02-22 12:15:58 +0100

    fixed admin and login js

--- script/admin.js
+++ script/admin.js
@@ -5,3 +5,19 @@
 function createProfessor() {
     
 }
+
+function onRegister() {
+    setSessionStorage();
+    var user = document.getElementById("R_user").value
+    var pass = document.getElementById("R_pass").value
+    if(!user.includes("@stud.ntnu.no")){
+        user = user + "@stud.ntnu.no";
+    }
+    if(user != "" || pass != "" || pass.length > 4){
+        firebase.auth().createUserWithEmailAndPassword(user, pass).catch(
+            error => alert(error.message));
+    }else{
+        window.location.reload();
+        alert("Invalid information");
+    }
+};
