c9babc8443bf0935147582c4b536a23d0866c867
Mathias <Mathias@10.22.42.48>
2017-02-22 10:29:36 +0100

    Working subscribing and unsubscribing to subjects.

--- script/subscriptionLogic.js
+++ script/subscriptionLogic.js
@@ -5,46 +5,69 @@
 var userID = "X3SJ2TMw3yMwz04fPoxSSuZCxW13";
 
 //Function for a student to subscribe to a subject
-function subscribeToSubject(user, subjectId){
+function subscribeToSubject(studentId, subjectId, callback){
 	var dbRef = firebase.database().ref();
-	if(user && subjectId){
-		dbRef.child("users/students/" + user.uid).push().set({
+	if(studentId && subjectId){
+		dbRef.child("users/students/" + studentId + "/subscriptions").push().set({
 			subjectId: "" + subjectId
-		})
-	}
-}
-
-//Function for a student to unsubscribe from a lecture
-function unsubscribeFromLecture(user, lectureID){
-	var dbRef = firebase.database().ref();
-	if(user && lectureID){
-		dbRef.child("userSubscription/" + user.uid + "/" + lectureID).remove()
-	}
-}
-
-//Fetches the lectures that a student has subscribed to
-function fetchLectures(user){
-	var dbRef = firebase.database().ref();
-	var arrayOfLectures = [];
-	if(user){
-		dbRef.child("userSubscription/" + user.uid).on("value", function(snapshot){
-			console.log(snapshot.val());
+		}).then(function(){
+			callback;
 		});
 	}
+	
 }
 
-//Searches for a subject based on a subject ID to check for its existence
-function verifySubject(subjectId){
-	console.log(subjectId);
-	var snap, returnstate = false;
+//Function for a student to unsubscribe from a subject
+function unsubscribeFromSubject(studentId, subjectId, callback){
+	var dbRef = firebase.database().ref();
+	if(studentId && subjectId){
+		dbRef.child("users/students/" + studentId + "/subscriptions").once("value", function(snapshot){
+			snapshot.forEach(function(childsnap){
+				if(childsnap.val().subjectId == subjectId){
+					dbRef.child("users/students/" + studentId + "/subscriptions/" + childsnap.key).remove();
+				}
+			})
+		}).then(function(){
+			callback;
+		});
+		
+		
+	}
+}
+
+//Gets a list of the subjects that the student has subscribed to
+function getSubscribedSubjects(studentId, callback){
+	var dbRef = firebase.database().ref();
+	var subjectIdList = [];
+	dbRef.child("users/students/" + studentId + "/subscriptions").once("value", function(snapshot){
+		snapshot.forEach(function(childsnap){
+			subjectIdList.push(childsnap.val().subjectId);
+		}).then(function(){
+			callback(subjectIdList);
+		});
+	});
+}
+
+function createSubjectList(subjectIdList){
 	
-	while(!returnstate){
-		returnstate, snap = getSubject(subjectId);
+	for(subjectId in subjectIdList){
+		var td1 = document.createElement("TD");
+		var td2 = document.createElement("TD");
+		var tr = document.createElement("TR");
+		var text = document.createElement("TEXTBOX");
+		var btn = document.createElement("BUTTON");
+		var list = document.getElementById("subListId");
+		
+		tr.style = "background-color: gray";
+		text.innerHTML = subjectId;
+		td1.appendChild(text);
+		btn.innerText = "UNSUBSCRIBE";
+		btn.onclick = function(){unsubscribeFromSubject(userID, subjectId)};
+		td2.appendChild(btn);
+		tr.appendChild(td1);
+		tr.appendChild(td2);
+		list.appendChild(tr);
 	}
-	if(snap != null){
-		return true,true;
-	}
-	return true, false;
 }
 
 //Searches for a subject and displays a button to subscribe to the subject
@@ -55,30 +78,58 @@ function searchForAndDisplaySubject(subjectId){
 	});
 	
 }
+
 //Creates the button to subscribe to a lecture
 function handleSubject(subjectId, subject){
+	var area = document.getElementById("searchResultId");
+	var failure = document.getElementById("searchFailureId");
+	var text = document.getElementById("searchResultTextId");
+	var btn = document.getElementById("searchResultButtonId");
 	if(subject != null){
-		var btn = document.createElement("BUTTON");
-		btn.id = "subjectButtonId";
-		btn.innerHTML = subjectId + "\n";
-		getProfessor(subject.professorId, function(professorId, snapshot){
-			if(snapshot != null){					;
-				subjectButtonId.innerHTML += "Lecturer: " + snapshot.val().username;
-			}
-		})
-		btn.onclick = subscribeToSubject(userID, subjectId);
-		subjectsBar.appendChild(btn);
+		updateSubjectButton(subjectId);
 	}
 	else{
-		searchFailureId.innerHTML = subjectId + " is not a registered subject.";
+		area.style.visibility = "hidden";
+		failure.style.visibility = "visible";
+		failure.innerHTML = subjectId + " is not a registered subject.";
 	}
 }
 
+function updateSubjectButton(subjectId, area = document.getElementById("searchResultId"), btn = document.getElementById("searchResultButtonId"), failure = document.getElementById("searchFailureId"), text = document.getElementById("searchResultTextId")){
+	checkIfSubscribed(userID, subjectId, function(isSubscribed){
+		area.style.visibility = "visible";
+		btn.innerHTML = subjectId + "\n";
+		failure.style.visibility = "hidden";
+		text.innerHTML = subjectId;
+		if(isSubscribed){
+			btn.innerHTML = "UNSUBSCRIBE";
+			btn.onclick = function(){unsubscribeFromSubject(userID, subjectId)};
+		}
+		else{
+			btn.innerHTML = "SUBSCRIBE";
+			btn.onclick = function(){subscribeToSubject(userID, subjectId)};
+		}
+	});
+}
+
+//Returns a professor based on it's id, with a callback function
 function getProfessor(professorId, callback){
 	var dbRef = firebase.database().ref();
 	dbRef.child("users/professors/" + professorId).once("value", function(snapshot){
-		console.log("1. " + snapshot.val());
 		callback(professorId, snapshot);
+	});
+}
+
+//Checks if the student has subscribed to the subjects
+function checkIfSubscribed(studentId, subjectId, callback){
+	var dbRef = firebase.database().ref();
+	dbRef.child("users/students/" + studentId + "/subscriptions").once("value", function(snapshot){
+		var isSubscribed = false;
+		isSubscribed = snapshot.forEach(function(childsnap){
+			if(childsnap.val().subjectId == subjectId)
+				return true;
+		});
+		callback(isSubscribed);
 	});
 }
 
@@ -88,24 +139,14 @@ function getProfessor(professorId, callback){
 */
 function getSubject(subjectId, callback){
 	var dbRef = firebase.database().ref();
-	var snap;
-	console.log("1");
 	dbRef.child("subjects").once("value", function(snapshot){
-		console.log("2");
-		var subject = snapshot.forEach(function(childsnap){
-			console.log("3");
+		snapshot.forEach(function(childsnap){
 			if(childsnap.val().subjectId == subjectId){
-				console.log("4");
-				return childsnap;
+				callback(subjectId, childsnap);
 			}
-			console.log("5");
 		});
-		console.log("6");
-		callback(subjectId, subject);
-		console.log("7");
 		
 	});
-	console.log("8");
 }
 
 function trialFunction(){
