2e5855185b898c43dd0abaf380a172acf321f3ba
Morten Mulvik <mortenmu@stud.ntnu.no>
2017-03-01 12:08:09 +0100

    New lecture form
    Added script and htmlform to create new lecture (not done!!)

--- script/professor.js
+++ script/professor.js
@@ -34,4 +34,29 @@ function getUserNamerCallback(username){
   alertOfChange("Welcome, " + navn + "!")
 }
 
+function newLecture() {
+	var subject = getSubject();
+	var day = getDate();
+	
+	// create new lecture with the supplied info and add to database
+	firebase.database().ref("lectures/" + subject).set({
+		date: day 
+	});
+}
 
+function getSubject() {
+	// get the correct subject code and semester
+	var subject = document.getElementById("subjectName").value;
+	var code = document.getElementById("subjectCode").value;
+	var semester = document.getElementById("semester").value;
+	
+	
+	return "nothing";
+}
+
+function getDate() {
+	// get date from create lecture form
+	
+	// if no date supplied from form use current date
+	return new Date();
+}
