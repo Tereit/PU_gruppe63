ed4b4235a2c6fedc2c3559c9ad39df7284021fe7
Morten Mulvik <mortenmu@stud.ntnu.no>
2017-03-01 12:52:04 +0100

    fixed infinate login loop bug

--- script/redirect.js
+++ script/redirect.js
@@ -1,6 +1,11 @@
 function checkLoginStatus() {
-	if(!firebase.auth().currentUser) {
-		window.location = "index.html";
-	}
+	firebase.auth().onAuthStateChanged(user => {
+		if(user){
+			// do nothing
+		}else{
+			console.log("not logged in");
+			window.location = "index.html";
+		}
+	});
 }
 checkLoginStatus();
