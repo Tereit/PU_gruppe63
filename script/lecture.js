a313b0b2fe947db2618559e7e3409b3cfea119e1
Mathias <Mathias@Mathias-PC>
2017-03-06 12:34:10 +0100

    lecture.js functionality, questions and answers

--- script/lecture.js
+++ script/lecture.js
@@ -3,29 +3,50 @@
  */
 
 //Creates a new question in a lecture
-function createQuestion(questionText, author, lectureId, date){
-	var questionId = dbRef.child("questions").push({
-		'lectureId': lectureId,
-		'date': date,
+function createQuestion(questionText, postedBy, lectureId, date){
+	dbRef.child("lectures/" + lectureId + "/" + date + "/questions").push({
 		'questionText': questionText,
-		'postedBy': author
-	}).key();
-	dbRef.child("lectures/" + lectureId + "/" + date + "questions" + questionId).push({
-		'null': "null"
-	});
+		'postedBy': postedBy,
+		'upvoteCount': 0,
+		'isRecommended': false
+	})
 }
 
 //Gets all questions for a lecture
 function getQuestions(lectureId, date, callback){
-	var questions = []
 	dbRef.child("lecture/" + lectureId + "/" + date + "questions").once("value", function(snapshot){
-		var snap = snapshot;
 		snapshot.forEach(function(childsnap){
-			dbRef.child("questions" + childsnap.key).once("value", function(snap){
-				questions.push(snap);
-			});
+			callback(snapshot);
 		});
-	}).then(function(){
-		callback(questions);
+	});
+}
+
+//Get a specific question
+function getQuestion(questionId, lectureId, date, callback){
+	dbRef.child("lecture/" + lectureId + "/" + date + "questions/" + questionId).once("value", function(snapshot){
+		snapshot.forEach(function(childsnap){
+			callback(snapshot);
+		});
+	});
+}
+
+//Answer a specific question
+function answerQuestion(questionId, lectureId, date, answerText, answeredBy){
+	getQuestion(questionId, lectureId, date, function(question){
+		question.child("answers").push({
+			'answerText': answerText,
+			'answeredBy': answeredBy,
+			'upvoteCount': 0,
+			'isRecommended': false
+		})
+	});
+}
+
+//Gets all answers for a question
+function getQuestions(questionId ,lectureId, date, callback){
+	dbRef.child("lecture/" + lectureId + "/" + date + "/question/" + questionId + "/answers").once("value", function(snapshot){
+		snapshot.forEach(function(childsnap){
+			callback(snapshot);
+		});
 	});
 }
