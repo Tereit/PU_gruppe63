89e078fb73a818f69776503915c515e301da6d22
Mathias <Mathias@Mathias-PC>
2017-03-06 15:33:24 +0100

    More functions for lecture.js

--- script/lecture.js
+++ script/lecture.js
@@ -31,6 +31,79 @@ function getQuestion(questionId, lectureId, callback){
 	});
 }
 
+//Updates the upvotes for the question
+function upvoteQuestion(questionId, lectureId, userId){
+	dbRef.child("questions/" + lectureId + "/" + questionId + "/upvotedB/" + userId).push({
+		'null': "null"
+	});
+}
+
+//Removes the upvote on a question
+function removeUpvoteQuestion(questionId, lectureId, userId){
+	dbRef.child("questions/" + lectureId + "/" + questionId + "/upvotedB/" + userId).remove();
+}
+
+//Set a listener for the lecture feed
+function questionFeedListener(lectureId){
+	var questionList = []
+	var listener = ChildEventListener()
+	dbRef.child("questions/" + lectureId).on("child_added", function(question){
+		newQuestion(question);
+	});
+	dbRef.child("questions/" + lectureId).on("child_removed", function(question){
+		deleteQuestionList(question);
+	});
+}
+
+//Updates the question list for the lecture
+function updateQuestion(questionList){
+	var qList = document.getElementById("questionList")
+	var qMain = document.createElement("VBOX")
+
+	var quest = document.createElement("HBOX")
+	var text = document.createElement("TEXTBOX")
+	text.innerHTML = question.val().questionText + "\t Posted By: " + question.val().postedBy + "\t " + question.child("answers").once(function(ans){return ans.numChildren});
+
+	quest.appendChild(text)
+	qMain.appendChild(quest)
+	var answers = document.createElement("VBOX")
+	question.child("answers").forEach(function(answer){
+		var ans = document.createElement("TEXTBOX")
+		ans = answer.val().answerText + "\t Posted By: " + answer.val().answeredBy
+		answers.append()
+	});		
+}
+
+function newQuestion(question){
+	var qList = document.getElementById("questionList")
+	var qMain = document.createElement("VBOX")
+	qMain.id = question.key
+	var quest = document.createElement("HBOX")
+	var text = document.createElement("TEXTBOX")
+	var div = document.createElement("DIV")
+	qMain.appendChild(div)
+	text.innerHTML = question.val().questionText + "\t Posted By: " + question.val().postedBy + "\t " + question.child("answers").once(function(ans){return ans.numChildren});
+
+	quest.appendChild(text)
+	qMain.appendChild(quest)
+	var answers = document.createElement("VBOX")
+	question.child("answers").forEach(function(answer){
+		var ans = document.createElement("TEXTBOX")
+		ans = answer.val().answerText + "\t Posted By: " + answer.val().answeredBy
+		answers.append()
+	});	
+	
+	qMain.appendChild(answers)
+	qList.append(qMain)
+	
+}
+
+function deleteQuestion(question){
+	var qList = document.getElementById("questionList")
+	var quest = document.getElementById(question.key)
+	qList.removeChild(quest)
+}
+
 //Answer a specific question
 function answerQuestion(questionId, lectureId, answerText, answeredBy){
 	getQuestion(questionId, lectureId, function(question){
